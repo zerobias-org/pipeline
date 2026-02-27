edc335241bd3e39a2232bfaddd9c04ef79d9e5a9
Daniel Rojas <drojas@zerobias.com>
2026-02-27 15:35:32 -0600

    feat: initial pipeline repo with agentskills pipeline

---
+++ scripts/publish.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+set -e
+set -x
+
+NAME=$(jq -r '.name' package.json)
+VERSION=$(jq -r '.version' package.json)
+
+LATEST=$(npm view $NAME dist-tags.latest) || echo ""
+SKIP_DATALOADER=$(gh pr view --json labels | jq '.labels | any(.name == "skip-dataloader")')
+
+if [ "$VERSION" != "$LATEST" ]; then
+  NPM_TOKEN=$READ_TOKEN npm i
+  npm shrinkwrap
+  npm publish
+
+  if [ "$SKIP_DATALOADER" = "true" ]; then
+    npm dist-tag add $NAME@$VERSION skip-dataloader
+  else
+    npm dist-tag rm $NAME@$VERSION skip-dataloader || echo 'skip-dataloader tag not present'
+  fi
+fi
